f13f399f527e2319106f0206c9b2e26da0b1b682
Eriarer <eriarer_messiah@hotmail.com>
2022-11-14 08:40:27 -0600

    CEstudiante Herencia Persona

---
+++ C++/Programacion/Tareas/U3/022Persona/CEstudiante.h
@@ -0,0 +1,34 @@
+#include "CPersona.h"
+
+class CEstudiante : protected CPersona {
+   protected:
+    string escuela;
+    float promedio;
+
+   public:
+    //=============CONSTRUCTOR=============//
+    CEstudiante();
+    CEstudiante(string escuela, float promedio, string nombre, int edad);
+    CEstudiante(const CEstudiante& eobj, const CPersona& pobj);
+    //=============GET-SET=============//
+    inline void setEscuela(string nombre) { this->nombre = nombre; };
+    inline void setPromedio(int promedio) { this->promedio = promedio; };
+    inline string getEscuela() { return this->escuela; };
+    inline int getPromedio() { return this->promedio; };
+    //=============FUNCIONES=============//
+    void mostrar() override;
+};
+//=============CONSTRUCTOR=============//
+CEstudiante::CEstudiante() : CPersona() {
+    escuela = "";
+    promedio = 0;
+}
+CEstudiante::CEstudiante(string escuela, float promedio, string nombre, int edad) : CPersona(nombre, edad) {
+    this->escuela = escuela;
+    this->promedio = promedio;
+};
+CEstudiante::CEstudiante(const CEstudiante& eobj, const CPersona& pobj) : CPersona(pobj) {
+    this->escuela = eobj.escuela;
+    this->promedio = eobj.promedio;
+};
+//=============FUNCIONES=============//
