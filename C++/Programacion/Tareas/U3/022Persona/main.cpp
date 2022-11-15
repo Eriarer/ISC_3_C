c731e79a918d9e358e5c6b1d83b6baef7c747466
Eriarer <eriarer_messiah@hotmail.com>
2022-11-15 08:53:45 -0600

    main

--- C++/Programacion/Tareas/U3/022Persona/main.cpp
+++ C++/Programacion/Tareas/U3/022Persona/main.cpp
@@ -1,9 +1,20 @@
-#include "CPersona.h"
+#include "CEstudiante.h"
 
 int main() {
-    CPersona yo;
-    cin >> yo;
-    cout << "\t<YO>\n"
-         << yo;
+    CPersona yo("Eriarer", 20);
+    CEstudiante yoE("UAA", 8.7, yo);
+    yoE.mostrar();
+    cout << endl;
     return 0;
 };
+/*
+#include "CTrabajador"
+
+int main() {
+    CPersona yo("Eriarer", 20);
+    CTrabajador yoT("Twitch", 500, yo);
+    yoE.mostrar();
+    cout << endl;
+    return 0;
+};
+*/
